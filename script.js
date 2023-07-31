1fc0f678b9256aa14fdf92ffe66d2c263070da86
vishalsinh <46310417+vishalsinh9@users.noreply.github.com>
2023-07-31 18:59:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,54 @@
-function updateDateTime() {
-    const now = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const dateTimeElement = document.getElementById('real-time');
-    const dateElement = document.getElementById('real-date');
-    dateTimeElement.textContent = now.toLocaleTimeString('en-US');
-    dateElement.textContent = now.toLocaleDateString('en-US', options);
+// Assuming you have obtained your API key from the weather API provider
+const apiKey = 'YOUR_API_KEY';
+const weatherAPIUrl = 'https://api.weatherapi.com/v1/current.json';
+
+// Function to fetch weather data from the API
+async function fetchWeatherData(latitude, longitude) {
+    try {
+        const response = await fetch(`${weatherAPIUrl}?key=${apiKey}&q=${latitude},${longitude}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
 }
 
-function init() {
-    updateDateTime();
-    setInterval(updateDateTime, 1000);
+// Function to initialize the map
+function initMap() {
+    // Initialize the map (you need to use a valid API key for Google Maps)
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 56.1304, lng: -106.3468 }, // Default center for Canada
+        zoom: 4 // Adjust the zoom level as needed
+    });
+
+    // Add markers to the map with weather information
+    // You'll need to fetch latitude and longitude for specific cities/locations
+    const cities = [
+        { name: 'Toronto', lat: 43.651070, lng: -79.347015 },
+        { name: 'Vancouver', lat: 49.2827, lng: -123.1207 },
+        // Add more cities as needed...
+    ];
+
+    cities.forEach(city => {
+        const marker = new google.maps.Marker({
+            position: { lat: city.lat, lng: city.lng },
+            map: map,
+            title: city.name
+        });
+
+        fetchWeatherData(city.lat, city.lng)
+            .then(data => {
+                if (data) {
+                    // Display weather information in the marker's info window
+                    const infoWindow = new google.maps.InfoWindow({
+                        content: `<strong>${city.name}</strong><br>Temperature: ${data.current.temp_c}°C<br>Condition: ${data.current.condition.text}`
+                    });
+
+                    marker.addListener('click', () => {
+                        infoWindow.open(map, marker);
+                    });
+                }
+            });
+    });
 }
-
-init();
-const quotes = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "Believe you can and you're halfway there. - Theodore Roosevelt",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-    "Strive not to be a success, but rather to be of value. - Albert Einstein",
-    "The best way to predict the future is to create it. - Peter Drucker",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-    "The only person you are destined to become is the person you decide to be. - Ralph Waldo Emerson",
-    "Opportunities don't happen. You create them. - Chris Grosser",
-    "In order to succeed, we must first believe that we can. - Nikos Kazantzakis"
-];
-
-function updateQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const quoteElement = document.getElementById('quote');
-    quoteElement.textContent = quotes[randomIndex];
-}
-
-function init() {
-    updateDateTime();
-    updateQuote(); // Initial quote
-    setInterval(updateDateTime, 1000);
-    setInterval(updateQuote, 15000); // Update quote every 15 seconds
-}
-
-init();
-function simulateRealTimeData() {
-    // Simulate real-time data for NSE and BSE stocks (Placeholder data)
-    const nsePrice = Math.random() * 1000 + 500;
-    const nseVolume = Math.random() * 100000 + 50000;
-    const bsePrice = Math.random() * 800 + 200;
-    const bseVolume = Math.random() * 50000 + 20000;
-
-    // Update NSE stock details
-    document.getElementById('nse-price').textContent = nsePrice.toFixed(2);
-    document.getElementById('nse-volume').textContent = nseVolume.toFixed(0);
-
-    // Update BSE stock details
-    document.getElementById('bse-price').textContent = bsePrice.toFixed(2);
-    document.getElementById('bse-volume').textContent = bseVolume.toFixed(0);
-
-    // Calculate and update price indicators
-    const nseIndicator = nsePrice > 800 ? 'High' : 'Low';
-    const bseIndicator = bsePrice > 500 ? 'High' : 'Low';
-    document.getElementById('price-indicator').textContent = nseIndicator;
-    document.getElementById('price-indicator-bse').textContent = bseIndicator;
-}
-
-function init() {
-    simulateRealTimeData(); // Initial data
-    setInterval(simulateRealTimeData, 3000); // Update data every 3 seconds (simulated real-time)
-}
-
-init();
